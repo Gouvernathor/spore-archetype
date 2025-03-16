2e9dbe88a570cbc76deb1e0be11006cf8733f43a
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-03-16 18:37:59 +0100

    Add philosophies

---
+++ philosophies.ts
@@ -0,0 +1,25 @@
+import { Archetype } from "./archetypes";
+
+export enum Philosophy {
+    Chance,
+    Order,
+    Life,
+    Force,
+    Science,
+    Harmony,
+    Prosperity,
+    Faith,
+}
+
+export const philosophyByArchetype = new Map([
+    [Archetype.Bard, Philosophy.Chance],
+    [Archetype.Diplomat, Philosophy.Order],
+    [Archetype.Ecologist, Philosophy.Life],
+    [Archetype.Knight, Philosophy.Force],
+    [Archetype.Scientist, Philosophy.Science],
+    [Archetype.Shaman, Philosophy.Harmony],
+    [Archetype.Trader, Philosophy.Prosperity],
+    [Archetype.Wanderer, Philosophy.Order],
+    [Archetype.Warrior, Philosophy.Force],
+    [Archetype.Zealot, Philosophy.Faith],
+]);
