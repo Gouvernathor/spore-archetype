276b5cef9076e61d1bed3e08d226400b4a3d5afb
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-03-16 18:31:57 +0100

    Generate sequences

--- archetypes.ts
+++ archetypes.ts
@@ -145,3 +145,27 @@ function getHalf(counter: Map<CardColor, number>): Archetype.Diplomat | Archetyp
     }
     return null;
 }
+
+export function* generateAllValidSequences() {
+    for (const cell of [CellCard.Skipped, CellCard.Carnivore, CellCard.Herbivore, CellCard.Omnivore]) {
+        for (const creature of [CreatureCard.Skipped, CreatureCard.Predator, CreatureCard.Social, CreatureCard.Adaptable]) {
+            if (creature === CreatureCard.Skipped && cell !== CellCard.Skipped) {
+                continue;
+            }
+
+            for (const tribal of [TribalCard.Skipped, TribalCard.Aggressive, TribalCard.Friendly, TribalCard.Industrious]) {
+                if (tribal === TribalCard.Skipped && creature !== CreatureCard.Skipped) {
+                    continue;
+                }
+
+                for (const civilization of [CivilizationCard.Skipped, CivilizationCard.Military, CivilizationCard.Religious, CivilizationCard.Economic]) {
+                    if (civilization === CivilizationCard.Skipped && tribal !== TribalCard.Skipped) {
+                        continue;
+                    }
+
+                    yield [cell, creature, tribal, civilization] as Sequence;
+                }
+            }
+        }
+    }
+}
