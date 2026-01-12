3dea1477d2f7695610a4dab99e61ca0cee21bc16
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-01-12 22:40:56 +0100

    docstring

--- src/archetypes/index.ts
+++ src/archetypes/index.ts
@@ -1,6 +1,13 @@
 import { Archetype } from "./base";
 import { Sequence } from "./sequence";
 
+/**
+ * if you have three of the same color, or all of the same color, you are pure of that color
+ * elif you have one of each exactly, you are pure of the last (civ) one
+ * elif you have one card for each color but one more of one color (1-1-2), you are a tendency of that color
+ * elif you have cards of exactly two colors with no 3 cards of the same color, you are half of those two colors
+ * @param nullIfInvalid if the sequence is inconsistent and this is true, return null instead of throwing an error
+ */
 export declare function getArchetype(
     sequence: Sequence,
     nullIfInvalid?: false,
