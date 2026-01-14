cce136d920384da8a238cf00655ec0f2c9fa8b1a
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-01-14 23:22:15 +0100

    docstrings for the archetypes

--- src/archetypes/base.ts
+++ src/archetypes/base.ts
@@ -1,18 +1,45 @@
 export enum Archetype {
+    /**
+     * The archetype without any card.
+     */
     Wanderer,
 
-    // meta-archetype : pure
+    /**
+     * A Pure archetype, maximizing the red cards.
+     */
     Warrior,
+    /**
+     * A Pure archetype, maximizing the green cards.
+     */
     Shaman,
+    /**
+     * A Pure archetype, maximizing the blue cards.
+     */
     Trader,
 
-    // meta-archetype : tendency
+    /**
+     * A Tendency archetype, tending towards red cards.
+     */
     Knight,
+    /**
+     * A Tendency archetype, tending towards green cards.
+     */
     Ecologist,
+    /**
+     * A Tendency archetype, tending towards blue cards.
+     */
     Bard,
 
-    // meta-archetype : half
+    /**
+     * A Half archetype, between green and blue cards.
+     */
     Diplomat,
+    /**
+     * A Half archetype, between red and blue cards.
+     */
     Scientist,
+    /**
+     * A Half archetype, between red and green cards.
+     */
     Zealot,
 }
