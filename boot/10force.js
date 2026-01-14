e0741825b140e7ab5d0640d2b981a049c3a2286d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-01-14 22:39:31 +0100

    change the type of the display values

--- boot/10force.js
+++ boot/10force.js
@@ -4,8 +4,8 @@ import { generate10force, setDocument } from "../dist/generate10force.js";
 import { archetypeCSSColors } from "../dist/display.js";
 
 setDocument(new JSDOM().window.document);
-const cssPropsObject = Object.fromEntries(Array.from(archetypeCSSColors, ([archetype, color]) => {
-    return [archetype, {fill: color}];
+const cssPropsObject = Object.fromEntries(Array.from(Object.keys(archetypeCSSColors), archetype => {
+    return [archetype, {fill: archetypeCSSColors[archetype]}];
 }));
 const svg = generate10force({
     propertiesPerArchetype: cssPropsObject,
--- src/display.ts
+++ src/display.ts
@@ -1,27 +1,29 @@
 import { Archetype } from "./archetypes";
 import { CardColor } from "./cards.js";
 
-/** @deprecated moved to colors.css */
-export const cardCSSColors = new Map([
-    [CardColor.Black, "#000000"],
-    [CardColor.Red, "#CE3F17"],
-    [CardColor.Green, "#4DEC5F"],
-    [CardColor.Blue, "#4ABDDA"],
-]);
+export const cardCSSColors: {
+    [Card in CardColor]: string;
+} = {
+    [CardColor.Black]: "#000000",
+    [CardColor.Red]: "#CE3F17",
+    [CardColor.Green]: "#4DEC5F",
+    [CardColor.Blue]: "#4ABDDA",
+};
 
-/** @deprecated moved to colors.css */
-export const archetypeCSSColors = new Map([
-    [Archetype.Wanderer, "#939699"],
+export const archetypeCSSColors: {
+    [Arch in Archetype]: string;
+} = {
+    [Archetype.Wanderer]: "#939699",
 
-    [Archetype.Warrior, "#CE3F17"],
-    [Archetype.Shaman, "#4DEC5F"],
-    [Archetype.Trader, "#4ABDDA"],
+    [Archetype.Warrior]: "#CE3F17",
+    [Archetype.Shaman]: "#4DEC5F",
+    [Archetype.Trader]: "#4ABDDA",
 
-    [Archetype.Knight, "#CE469A"],
-    [Archetype.Ecologist, "#A3CE46"],
-    [Archetype.Bard, "#48BC8D"],
+    [Archetype.Knight]: "#CE469A",
+    [Archetype.Ecologist]: "#A3CE46",
+    [Archetype.Bard]: "#48BC8D",
 
-    [Archetype.Diplomat, "#C6CB47"],
-    [Archetype.Scientist, "#5046CE"],
-    [Archetype.Zealot, "#A646CE"],
-]);
+    [Archetype.Diplomat]: "#C6CB47",
+    [Archetype.Scientist]: "#5046CE",
+    [Archetype.Zealot]: "#A646CE",
+};
