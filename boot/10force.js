78c106713a33dceb31f5b290de4a59df7a3ce49d
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-01-17 11:34:57 +0100

    simplify the script

--- boot/10force.js
+++ boot/10force.js
@@ -1,13 +1,7 @@
 import { JSDOM } from "jsdom";
 import * as fs from "fs";
 import { generate10force } from "../dist/generate10force.js";
-import { archetypeCSSColors } from "../dist/display.js";
 
 globalThis.document = new JSDOM().window.document;
-const cssPropsObject = Object.fromEntries(Array.from(Object.keys(archetypeCSSColors), archetype => {
-    return [archetype, {fill: archetypeCSSColors[archetype]}];
-}));
-const svg = generate10force({
-    propertiesPerArchetype: cssPropsObject,
-});
+const svg = generate10force();
 fs.writeFileSync("out/10force.svg", svg.outerHTML);
